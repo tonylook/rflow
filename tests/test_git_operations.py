1718e383d4d315f0c046309384e1b85c9ae612c0
tonylook <antonioalvino95@gmail.com>
2024-01-16 15:08:27 +0100

    tests updated

---
+++ tests/test_git_operations.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch
+
+from rflow.git_operations import (is_release_branch, get_main_branch_name)
+
+class TestGitOperations(unittest.TestCase):
+    def test_is_release_branch(self):
+        self.assertTrue(is_release_branch('release/v1.0.0'))
+        self.assertFalse(is_release_branch('main'))
+
+    @patch('rflow.git_operations.git.Repo', autospec=True)
+    def test_get_main_branch_name(self, mock_repo):
+        mock_repo.branches = ['main']
+        self.assertEqual(get_main_branch_name(mock_repo), 'main')
+
+        mock_repo.branches = ['master']
+        self.assertEqual(get_main_branch_name(mock_repo), 'master')
+
+        mock_repo.branches = ['fail']
+        with self.assertRaises(ValueError):
+            get_main_branch_name(mock_repo)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_version_operations.py
@@ -0,0 +1,67 @@
+import unittest
+import tempfile
+from unittest.mock import MagicMock
+import json
+import os
+
+import click
+
+from rflow.version_operations import (read_current_version, read_next_version, increment_major_version,
+                                 increment_minor_version, increment_patch_version, check_version_info_exists, init_version, get_latest_release_version)
+
+
+class TestVersionOperations(unittest.TestCase):
+    def setUp(self):
+        self.version_info = {
+            'currentVersion': '1.0.0',
+            'nextVersion': '1.0.1',
+        }
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.version_file = os.path.join(self.temp_dir.name, 'version.info')
+        with open(self.version_file, 'w') as f:
+            json.dump(self.version_info, f)
+
+        os.chdir(self.temp_dir.name)
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
+    def test_read_current_version(self):
+        self.assertEqual(read_current_version(), '1.0.0')
+
+    def test_read_next_version(self):
+        self.assertEqual(read_next_version(), '1.0.1')
+
+    def test_increment_major_version(self):
+        self.assertEqual(increment_major_version('1.2.3'), '2.0.0')
+
+    def test_increment_minor_version(self):
+        self.assertEqual(increment_minor_version('1.2.3'), '1.3.0')
+
+    def test_increment_patch_version(self):
+        self.assertEqual(increment_patch_version('1.2.3'), '1.2.4')
+
+    def test_check_version_info_exists(self):
+        self.tearDown()
+        with self.assertRaises(click.Abort):
+            check_version_info_exists()
+
+    def test_init_version(self):
+        self.assertEqual(init_version(), '1.0.0')
+
+
+    def test_get_latest_release_version(self):
+        class FakeBranch:
+            def __init__(self, name):
+                self.name = name
+
+        fake_repo = MagicMock()
+        fake_repo.branches = [
+            FakeBranch('release/v1.0.0'),
+            FakeBranch('release/v1.2.0'),
+            FakeBranch('release/v1.3.0'),
+        ]
+        self.assertEqual(get_latest_release_version(fake_repo), '1.3.0')
+
+if __name__ == '__main__':
+    unittest.main()
