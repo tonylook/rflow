8702788cf43e5e610796abf3ae8baf952bf5b337
tonylook <antonioalvino95@gmail.com>
2024-01-25 11:46:03 +0100

    updated python dependencies

--- setup.py
+++ setup.py
@@ -17,8 +17,10 @@ setup(
     include_package_data=True,
     install_requires=[
         'click==8.1.7',
-        'GitPython==3.1.40',
+        'GitPython==3.1.41',
         'semantic-version==2.10.0',
+        'gitdb==4.0.11',
+        'smmap==5.0.1'
     ],
     entry_points={
         'console_scripts': [
